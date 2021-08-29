c8a9a82ace1f5c4e5c999102038f2b826923ee41
Ayush Gupta <ayush.gupta2@cognizant.com>
2021-08-29 10:22:30 +0530

    project created

---
+++ src/components/basics/Restaurent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Restaurent = () => {
+    return (
+        <React.Fragment>
+            <h1>Hello Foodie...</h1>
+        </React.Fragment>
+    )
+}
+
+export default Restaurent
